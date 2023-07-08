d62d242abdc1efb2120cbe2901d6a1de7549ff99
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-07-08 22:08:41 +0900

    add

---
+++ download_checkpoints.py
@@ -0,0 +1,6 @@
+from util.gdrive_downloader import download_gdrive_file
+
+download_gdrive_file("./mtviton.pth","1XJTCdRBOPVgVTmqzhVGFAgMm2NLkw5uQ")
+download_gdrive_file("./discriminator_mtviton.pth","1Gi185XUAI3w4srReTbp3eIzkjFC51ym-")
+download_gdrive_file("./gen.pth","1T5_YDUhYSSKPC_nZMk2NeC-XXUFoYeNy")
+download_gdrive_file("./alex.pth","1FF3BBSDIA3uavmAiuMH6YFCv09Lt8jUr")
